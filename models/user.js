b81c70ccb893ee45c17eab31779be6ea7f5ded59
Zack Barovsky <57148341+zbarovsky@users.noreply.github.com>
2021-01-20 11:35:52 -0800

    fixed up users model for validations

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 'use strict';
+const bcrypt = require(bcrypt);
+
 const {
   Model
 } = require('sequelize');
@@ -10,17 +12,59 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      // TODO: WRITE ASSOCIATIONS 1-M (one user has many lists)
       // define association here
     }
   };
   user.init({
-    name: DataTypes.STRING,
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
-    age: DataTypes.INTEGER
-  }, {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [1,99],
+          msg: 'Name must be between 1 and 99 characters'
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        unique: true,
+        isEmail: {
+          msg: 'Invalid email address'
+        }
+      }
+      
+    },
+    username: {
+      type: DataTypes.STRING,
+      validate: {
+        unique: true,
+        len: {
+          args: [1,99],
+          msg: 'Username must be between 1 and 99 characters'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        is: ["^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"],
+        msg: 'Password must be at least 8 characters, and have one number and one special character'
+      }
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      validate: {
+        isNumeric: true,
+        msg: 'Please enter a number'
+      }
+    }
+  },
+  //  TODO: WRITE BCRYPT HOOK FOR HASH AND SALT PASSWORD
+
+  {
     sequelize,
     modelName: 'user',
   });
