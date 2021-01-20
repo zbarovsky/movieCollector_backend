13dc739ef70925705ea755e080bb6c85310b0984
Zack Barovsky <57148341+zbarovsky@users.noreply.github.com>
2021-01-20 10:46:10 -0800

    burned and recreated db to add a username for users

--- models/user.js
+++ models/user.js
@@ -15,6 +15,7 @@ module.exports = (sequelize, DataTypes) => {
   };
   user.init({
     name: DataTypes.STRING,
+    username: DataTypes.STRING,
     email: DataTypes.STRING,
     username: DataTypes.STRING,
     password: DataTypes.STRING,
